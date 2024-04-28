40365757c8b926b5f3e35846409b43fc0def05b1
carsondecker <carsondecker1205@gmail.com>
2024-04-28 17:40:22 -0500

    created createGrid to allow changing grid size

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,15 +1,31 @@
 // Get the div for the grid
 const grid = document.querySelector("#container");
 
-// Create grid
-for (let rows = 0; rows < 16; rows++) {
-    const row = document.createElement("div");
-    row.classList.add("row")
-    grid.appendChild(row);
+// Function to fill square
+const fill = (e) => { e.target.classList.add("active"); }
 
-    for (let squares = 0; squares < 16; squares++) {
-        const square = document.createElement("div");
-        square.classList.add("square");
-        row.appendChild(square);
+// Create grid
+const createGrid = (size) => {
+    // Clear the existing grid
+    grid.innerHTML = '';
+    
+    // Calculate the square size
+    const squareSize = (480 / size) - 2;
+
+    for (let rows = 0; rows < size; rows++) {
+        const row = document.createElement("div");
+        row.classList.add("row")
+        grid.appendChild(row);
+
+        for (let squares = 0; squares < size; squares++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            square.style.width = `${squareSize}px`;
+            square.style.height = `${squareSize}px`;
+            square.addEventListener("mouseover", fill);
+            row.appendChild(square);
+        }
     }
 }
+
+createGrid(16);
